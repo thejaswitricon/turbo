7726d0980240296b2600f61d5aa67888db2a9c94
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-06-26 12:59:44 +0530

    feat(root): initial

--- libs/lib-b/index.js
+++ libs/lib-b/index.js
@@ -1,2 +1,10 @@
-const who = require('@mono/lib-c');
-module.exports = 'Goodbye ' + who;
+const { response } = require('express')
+const express = require('express')
+const app = express()
+app.get('/', (req,res)=>{
+    res.send("home page")
+})
+app.listen(5000, ()=>{
+    console.log("Welcome to Javascript app1, Server is running on port 5000");
+})
+
--- libs/lib-c/index.js
+++ libs/lib-c/index.js
@@ -1,1 +1,10 @@
-module.exports = 'Mr. Noone';
+const { response } = require('express')
+const express = require('express')
+const app = express()
+app.get('/', (req,res)=>{
+    res.send("home page")
+})
+app.listen(5000, ()=>{
+    console.log("Welcome to Javascript app1, Server is running on port 5000");
+})
+
