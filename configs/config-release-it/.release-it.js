585e4db30de6b54379dfa922b5346279ed5546b9
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-06-26 15:21:51 +0530

    feat(root): initial

--- configs/config-release-it/.release-it.js
+++ configs/config-release-it/.release-it.js
@@ -1,5 +1,5 @@
 const version = '${version}';
-// const packageName = process.env.npm_package_name;
+const packageName = process.env.npm_package_name;
 const scope = packageName.split('/')[1];
 
 module.exports = {
